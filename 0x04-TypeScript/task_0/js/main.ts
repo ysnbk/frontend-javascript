ff0c679f8b990818814e2d79a3a9491cbed11448
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-13 21:15:41 +0100

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,18 @@
-
+interface  Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 21,
+  location: 'New York'
+};
+const student2: Student = {
+  firstName: 'Leo',
+  lastName: 'Messi',
+  age: 38,
+  location: 'Barcelona'
+};

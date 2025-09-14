9b60c13f11e6aa93d9a5dedfacc009a436da4d80
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-14 17:35:48 +0100

    task 9

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -6,7 +6,7 @@ namespace Subjects{
     getRequirements(): string{
         return "Here is the list of requirements for Cpp"
     }
-    getAvailableTeacher():string{
+    getAvailableTeacher(): string{
        if (this.teacher && this.teacher.firstName && this.teacher.experienceTeachingC > 0) {
         return `Available Teacher: ${this.teacher.firstName}`;
       }

403e63fc443d78fa9db2b6dee3d08ac2c9dcdb81
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-14 16:06:01 +0100

    Create main.ts

---
+++ 0x04-TypeScript/task_2/main.ts
@@ -0,0 +1,49 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+interface TeacherInterface {
+  workFromHome():string;
+  getCoffeeBreak():string;
+  workTeacherTasks():string;
+}
+
+class Director implements DirectorInterface {
+  function workFromHome(){
+    return "Working from home";
+  }
+  function getCoffeeBreak(){
+    return "Getting a coffee break";
+  }
+function workDirectorTasks(){
+  return "Getting to director tasks";
+}
+}
+class Teacher implements TeacherInterface {
+  function workFromHome(){
+    return "Cannot work from home";
+  }
+  function getCoffeeBreak(){
+    return "Cannot have a break";
+  }
+function workTeacherTasks(){
+  return "Getting to work";
+}
+}
+function createEmployee(salary:number){
+  if (salary < 500) return new Teacher()
+  return new Director()
+}
+function isDirector(employee){
+  
+}
+function executeWork(employee: Teacher | Director): string {
+  if (employee instanceof Director) {
+    return employee.workDirectorTasks();
+  } else if (employee instanceof Teacher) {
+    return employee.workTeacherTasks();
+  } else {
+    throw new Error("Unknown employee type");
+  }
+}

033260a5c4c21af3e065ee7ab65f7cdd76f0d8d6
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-14 16:10:03 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -35,7 +35,7 @@ function createEmployee(salary:number){
   if (salary < 500) return new Teacher()
   return new Director()
 }
-function isDirector(employee: Teacher | Director): employee is Director {
+export function isDirector(employee: Teacher | Director): employee is Director {
   return employee instanceof Director;
 }
 function executeWork(employee: Teacher | Director): string {

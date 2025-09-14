22e27b985cc56185cd3f72cbe18a583021981f39
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-14 17:35:10 +0100

    task 9

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -2,7 +2,7 @@ namespace Subjects{
     export interface Teacher {
     experienceTeachingC?: number;
   }
-  export class Cpp extends Subject{
+  class Cpp extends Subject{
     getRequirements(): string{
         return "Here is the list of requirements for Cpp"
     }
